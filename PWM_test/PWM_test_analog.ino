719b3e98cb39dd527fd60ac0f324b905ea40793a
mac7823 <mac7823@mail.rit.edu>
2015-02-08 17:10:48 -0500

    adding analog write code

---
+++ PWM_test/PWM_test_analog.ino
@@ -0,0 +1,35 @@
+// Initial PWM test code.
+// This code uses analog write, more specialized code may be needed due to interrupt usage.  
+// Frequency and pin may need adjustment as well.
+// Mitchell Cook
+
+// initialize pwm pin, using digital write this can be any digital out pin
+int PWM_output_pin = 11;
+
+
+void setup(){
+	Serial.begin(115200);
+	Serial.print("Initializing PWM output on pin: ");
+	Serial.println(PWM_output_pin);
+	
+	pinMode(PWM_output_pin, OUTPUT);
+}
+
+void loop(){
+	
+	Serial.print("Running at 0%");
+	analogWrite(PWM_output_pin, 0);
+	
+	Serial.print("Running at 25%");
+	analogWrite(PWM_output_pin, 64);
+	
+	Serial.print("Running at 50%");
+	analogWrite(PWM_output_pin, 127);
+	
+	Serial.print("Running at 75%");
+	analogWrite(PWM_output_pin, 191);
+	
+	Serial.print("Running at 100%");
+	analogWrite(PWM_output_pin, 255);
+	
+}
