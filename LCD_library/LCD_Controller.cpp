3c9026e9d99d49f274bcd7dad495270edc1f4ece
Marit Brocker <mlb8494@mail.rit.edu>
2015-04-07 23:34:12 -0400

    Define framework for LCD controller library, add required LCD library files.

---
+++ LCD_library/LCD_Controller.cpp
@@ -0,0 +1,89 @@
+/*
+ *
+ *	LCD_Controller.cpp
+ *
+ *	Team: Marit Brocker, Mitch Cook, Alex Wilson, Caleb Disbrow
+ *	Creation Date: 14 March 2015
+ *	Project: RIT Senior Design 2014-2015 -- Camera Stabilization
+ *
+ *	Description: Funtionality for the LCD controller for a 3-axis camera stabilization system 
+ *				 menu displayed on an ST7565 LCD (128x64) with RGB backlight. Includes full 
+ *				 functionality for menu navigation, axis manipulation, and system configuration.
+ *
+ */
+
+
+
+#include "LCD_Controller.h"
+
+
+void initialize_LCD(){
+
+}
+
+
+void toggle_interrupt_handler(){
+
+}
+
+
+void button_interrupt_handler(){
+
+}
+
+
+void respond_to_action(){
+
+}
+
+
+void u8g_prepare(){
+
+}
+
+
+void draw_home( int cur_menu_index ){
+
+}
+
+
+void draw_sys( int cur_menu_index ){
+
+}
+
+
+void draw_axis_select( int cur_menu_index ){
+
+}
+
+
+void draw_axis_options( int cur_menu_index, char[] header ){
+
+}
+
+
+void draw_axis_control(){
+
+}
+
+
+void draw(){
+
+}
+
+
+void handle_select( int command ){
+
+}
+
+
+// CONTROL FUNCTIONS
+void unlock_axis( int axis ){
+
+}
+
+
+void reset_axis( int axis ){
+
+}
+
