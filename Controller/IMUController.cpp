4acdb3a9a88e868bbf483902e18fb5a90519ee63
U-MAIN\mac7823 <mac7823@HDC-26.main.ad.rit.edu>
2015-04-20 15:51:40 -0400

    proposal for debug code in comments

--- Controller/IMUController.cpp
+++ Controller/IMUController.cpp
@@ -12,6 +12,18 @@
  */
  /***************************** Preprocessor Flags ****************************/
 #define DEBUG
+
+//possible implementation of debug code
+//#ifdef DEBUG
+// #define DEBUG_PRINT(x)     Serial.print (x)
+// #define DEBUG_PRINTDEC(x)     Serial.print (x, DEC)
+// #define DEBUG_PRINTLN(x)  Serial.println (x)
+//#else
+// #define DEBUG_PRINT(x)
+// #define DEBUG_PRINTDEC(x)
+// #define DEBUG_PRINTLN(x) 
+//#endif
+
  /***************************** Include Files *********************************/
 #include "IMUController.h"
 
