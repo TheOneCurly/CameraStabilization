12b0916bfb24e97999f2f2c7e506bbb0112bdf2e
U-MAIN\mac7823 <mac7823@HDC-26.main.ad.rit.edu>
2015-04-28 15:55:22 -0400

    remove interrupts, comment out a lot of code.  initialize the two imu's and then just poll them

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -43,9 +43,6 @@ customPWM motorPinz(PWM_pin_z);
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    //#ifndef TWI_FREQ
-    //#define TWI_FREQ 400000L
-    //#endif
     #include "Wire.h"
 #endif
 
@@ -58,17 +55,6 @@ float* angle_values = (float*) malloc(3*sizeof(float));
 float* error_angle_values = (float*) malloc(3*sizeof(float));
 float* angle_values_init = (float*) malloc(3*sizeof(float));
 float* error_angle_values_init = (float*) malloc(3*sizeof(float));
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-//
-void dmp0DataReady() {
-    imu.mpuInterrupt = true;
-}
-
-void dmp1DataReady() {
-    imu_error.mpuInterrupt = true;
-}
 
 // ================================================================
 // ===                      INITIAL SETUP                       ===
@@ -96,9 +82,6 @@ void setup() {
     digitalWrite(enable_y, LOW);
     digitalWrite(enable_z, LOW);
     
-    attachInterrupt(IMU0Interrupt, dmp0DataReady, RISING);
-    attachInterrupt(IMU1Interrupt, dmp1DataReady, RISING);
-    
     bool imu_ready = false;
     bool imu_error_ready = false;
     bool imu_valid = false;
@@ -110,42 +93,42 @@ void setup() {
     imu_error.init();
     imu_error.poll(error_angle_values);
     
-    while(!imu_ready || !imu_error_ready){
-      delay(15); // needs small delay for init DO NOT REMOVE >:( 
-      imu_valid = false;
-      imu_error_valid = false;
-      angle_values_init[0] = angle_values[0];
-      angle_values_init[1] = angle_values[1];
-      angle_values_init[2] = angle_values[2];
-     
-      error_angle_values_init[0] = error_angle_values[0];
-      error_angle_values_init[1] = error_angle_values[1];
-      error_angle_values_init[2] = error_angle_values[2];
-      
-      imu_valid = imu.poll(angle_values);
-      imu_error_valid = imu_error.poll(error_angle_values);
-      
-      if(imu_valid && !imu_ready){ 
-        if((abs(angle_values[0] - angle_values_init[0])<ANGLE_INIT_THRESHOLD && abs(angle_values[1] - angle_values_init[1])<ANGLE_INIT_THRESHOLD && abs(angle_values[2] - angle_values_init[2])<ANGLE_INIT_THRESHOLD)){
-          setBaseAngles(angle_values,0);
-          imu_ready = true;
-        }
-      }
-      
-      if(imu_error_valid && !imu_error_ready){
-        if((abs(error_angle_values[0] - error_angle_values_init[0])<ANGLE_INIT_THRESHOLD && abs(error_angle_values[1] - error_angle_values_init[1])<ANGLE_INIT_THRESHOLD && abs(error_angle_values[2] - error_angle_values_init[2])<ANGLE_INIT_THRESHOLD)){
-          setBaseAngles(error_angle_values,1);
-          imu_error_ready = true;
-        }
-      }
-    }
-    
-    // Enable movement
-    digitalWrite(enable_x, HIGH);
-    digitalWrite(enable_y, HIGH);
-    digitalWrite(enable_z, HIGH);
-    free(angle_values_init);
-    free(error_angle_values_init);
+//    while(!imu_ready || !imu_error_ready){
+//      delay(15); // needs small delay for init DO NOT REMOVE >:( 
+//      imu_valid = false;
+//      imu_error_valid = false;
+//      angle_values_init[0] = angle_values[0];
+//      angle_values_init[1] = angle_values[1];
+//      angle_values_init[2] = angle_values[2];
+//     
+//      error_angle_values_init[0] = error_angle_values[0];
+//      error_angle_values_init[1] = error_angle_values[1];
+//      error_angle_values_init[2] = error_angle_values[2];
+//      
+//      imu_valid = imu.poll(angle_values);
+//      imu_error_valid = imu_error.poll(error_angle_values);
+//      
+//      if(imu_valid && !imu_ready){ 
+//        if((abs(angle_values[0] - angle_values_init[0])<ANGLE_INIT_THRESHOLD && abs(angle_values[1] - angle_values_init[1])<ANGLE_INIT_THRESHOLD && abs(angle_values[2] - angle_values_init[2])<ANGLE_INIT_THRESHOLD)){
+//          setBaseAngles(angle_values,0);
+//          imu_ready = true;
+//        }
+//      }
+//      
+//      if(imu_error_valid && !imu_error_ready){
+//        if((abs(error_angle_values[0] - error_angle_values_init[0])<ANGLE_INIT_THRESHOLD && abs(error_angle_values[1] - error_angle_values_init[1])<ANGLE_INIT_THRESHOLD && abs(error_angle_values[2] - error_angle_values_init[2])<ANGLE_INIT_THRESHOLD)){
+//          setBaseAngles(error_angle_values,1);
+//          imu_error_ready = true;
+//        }
+//      }
+//    }
+//    
+//    // Enable movement
+//    digitalWrite(enable_x, HIGH);
+//    digitalWrite(enable_y, HIGH);
+//    digitalWrite(enable_z, HIGH);
+//    free(angle_values_init);
+//    free(error_angle_values_init);
 }
 
 
@@ -159,31 +142,31 @@ void loop() {
     base_imu_flag = imu.poll(angle_values);
     error_imu_flag = imu_error.poll(error_angle_values);
 
-    //pid returns 3 duty cycles
-    //ignore cases where there was a fifo overflow
-    if( error_imu_flag && base_imu_flag ){
-      duty = PIDMovementCalc_withError(angle_values, error_angle_values);
-      
-     // debug
-      Serial.print(F("duty cycles: \t"));                               
-      Serial.print(duty[0]);
-      Serial.print(F("\t"));
-      Serial.print(duty[1]);
-      Serial.print(F("\t"));
-      Serial.println(duty[2]);
-      Serial.println(F(""));
-      
-      //set duty cycles
-      motorPinx.duty(duty[0]);
-      motorPiny.duty(duty[1]);
-      motorPinz.duty(duty[2]);
-      
-      free(duty);
-      //repeat
-    }
-    else{
-      Serial.println(F("Ignoring duty calculations because of fifo overflow."));
-    }
+//    //pid returns 3 duty cycles
+//    //ignore cases where there was a fifo overflow
+//    if( error_imu_flag && base_imu_flag ){
+//      duty = PIDMovementCalc_withError(angle_values, error_angle_values);
+//      
+//     // debug
+//      Serial.print(F("duty cycles: \t"));                               
+//      Serial.print(duty[0]);
+//      Serial.print(F("\t"));
+//      Serial.print(duty[1]);
+//      Serial.print(F("\t"));
+//      Serial.println(duty[2]);
+//      Serial.println(F(""));
+//      
+//      //set duty cycles
+//      motorPinx.duty(duty[0]);
+//      motorPiny.duty(duty[1]);
+//      motorPinz.duty(duty[2]);
+//      
+//      free(duty);
+//      //repeat
+//    }
+//    else{
+//      Serial.println(F("Ignoring duty calculations because of fifo overflow."));
+//    }
 }
 
 
--- Controller/IMUController.cpp
+++ Controller/IMUController.cpp
@@ -65,9 +65,9 @@ const int AVERAGE_COUNT = 6;
     
     #ifdef DEBUG
     // initialize serial communication
-    //Serial.begin(115200);
+              //Serial.begin(115200);
     
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    //while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
     // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
     // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
@@ -83,85 +83,85 @@ const int AVERAGE_COUNT = 6;
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-    // wait for ready
-    //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    //while (!Serial.available());                 // wait for data
-    //while (Serial.available() && Serial.read()); // empty buffer again
+            // wait for ready
+            //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+            //while (Serial.available() && Serial.read()); // empty buffer
+            //while (!Serial.available());                 // wait for data
+            //while (Serial.available() && Serial.read()); // empty buffer again
 
     // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
-
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
-    // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-
-        // enable Arduino interrupt detection
-        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        /*
-        if(interruptNum == 0){
-            attachInterrupt(0, dmp0DataReady, RISING);
-        }else{
-            attachInterrupt(1, dmp1DataReady, RISING);
-        }
-        */
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        Serial.println(" ");
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
-	#else
-	mpu.initialize();
-	if(!mpu.testConnection()){
-		return false;
-	}
-	devStatus = mpu.dmpInitialize();
-	
-	// supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-	
-	// make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        // turn on the DMP, now that it's ready
-        mpu.setDMPEnabled(true);
-
-        // enable Arduino interrupt detection
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-        
-        return true;
-    } else {
-        return false;
-    }
-	#endif
+            //    Serial.println(F("Initializing DMP..."));
+            //    devStatus = mpu.dmpInitialize();
+            //
+            //    // supply your own gyro offsets here, scaled for min sensitivity
+            //    mpu.setXGyroOffset(220);
+            //    mpu.setYGyroOffset(76);
+            //    mpu.setZGyroOffset(-85);
+            //    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+            //
+            //    // make sure it worked (returns 0 if so)
+            //    if (devStatus == 0) {
+            //        // turn on the DMP, now that it's ready
+            //        Serial.println(F("Enabling DMP..."));
+            //        mpu.setDMPEnabled(true);
+            //
+            //        // enable Arduino interrupt detection
+            //        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+            //        /*
+            //        if(interruptNum == 0){
+            //            attachInterrupt(0, dmp0DataReady, RISING);
+            //        }else{
+            //            attachInterrupt(1, dmp1DataReady, RISING);
+            //        }
+            //        */
+            //        mpuIntStatus = mpu.getIntStatus();
+            //
+            //        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+            //        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+            //        Serial.println(" ");
+            //        dmpReady = true;
+            //
+            //        // get expected DMP packet size for later comparison
+            //        packetSize = mpu.dmpGetFIFOPacketSize();
+            //    } else {
+            //        // ERROR!
+            //        // 1 = initial memory load failed
+            //        // 2 = DMP configuration updates failed
+            //        // (if it's going to break, usually the code will be 1)
+            //        Serial.print(F("DMP Initialization failed (code "));
+            //        Serial.print(devStatus);
+            //        Serial.println(F(")"));
+            //    }
+            //	#else
+            //	mpu.initialize();
+            //	if(!mpu.testConnection()){
+            //		return false;
+            //	}
+            //	devStatus = mpu.dmpInitialize();
+            //	
+            //	// supply your own gyro offsets here, scaled for min sensitivity
+            //    mpu.setXGyroOffset(220);
+            //    mpu.setYGyroOffset(76);
+            //    mpu.setZGyroOffset(-85);
+            //    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+            //	
+            //	// make sure it worked (returns 0 if so)
+            //    if (devStatus == 0) {
+            //        // turn on the DMP, now that it's ready
+            //        mpu.setDMPEnabled(true);
+            //
+            //        // enable Arduino interrupt detection
+            //        attachInterrupt(0, dmpDataReady, RISING);
+            //        mpuIntStatus = mpu.getIntStatus();
+            //
+            //        // get expected DMP packet size for later comparison
+            //        packetSize = mpu.dmpGetFIFOPacketSize();
+            //        
+            //        return true;
+            //    } else {
+            //        return false;
+            //    }
+            	#endif
 }
 
 /**
@@ -177,79 +177,89 @@ const int AVERAGE_COUNT = 6;
  ******************************************************************************/
 bool IMUController::poll(float* angle_values){
     ypr_count = 0;
+    int16_t ax, ay, az;
+    int16_t gx, gy, gz;
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        Serial.println(gz);
     
-    if (!dmpReady) return false;
-    
-    while(ypr_count < (AVERAGE_COUNT + 1)){
-      if (mpuInterrupt || fifoCount >= packetSize){  
-        // reset interrupt flag and get INT_STATUS byte
-        mpuInterrupt = false;
-        mpuIntStatus = mpu.getIntStatus();
-    
-        // get current FIFO count
-        fifoCount = mpu.getFIFOCount();
-        // check for overflow (this should never happen unless our code is too inefficient)
-        if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-            // reset so we can continue cleanly
-            mpu.resetFIFO();
-            
-            #ifdef DEBUG
-              Serial.println(F("FIFO overflow!"));
-            #endif
-            
-            return false;    
-        // otherwise, check for DMP data ready interrupt (this should happen frequently)
-        } else if (mpuIntStatus & 0x02) {
-            // wait for correct available data length, should be a VERY short wait
-            while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-    
-            // read a packet from FIFO
-            mpu.getFIFOBytes(fifoBuffer, packetSize);
-            
-            // track FIFO count here in case there is > 1 packet available
-            // (this lets us immediately read more without waiting for an interrupt)
-            fifoCount -= packetSize;
-            
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            if (ypr_count < AVERAGE_COUNT) {
-              ypr_avg[0] += ypr[0];
-              ypr_avg[1] += ypr[1];
-              ypr_avg[2] += ypr[2];
-              ypr_count ++;
-            }else{
-              #ifdef DEBUG
-                Serial.print(interruptNum);
-                Serial.print(F("\t ypr\t"));
-                Serial.print(ypr_avg[0] * 180/(AVERAGE_COUNT*M_PI));
-                Serial.print(F("\t"));
-                Serial.print(ypr_avg[1] * 180/(AVERAGE_COUNT*M_PI));
-                Serial.print(F("\t"));
-                Serial.println(ypr_avg[2] * 180/(AVERAGE_COUNT*M_PI));
-              #endif
-  
-              ypr_avg[0] = ypr_avg[0] * 180/(AVERAGE_COUNT*M_PI);
-              ypr_avg[1] = ypr_avg[1] * 180/(AVERAGE_COUNT*M_PI);
-              ypr_avg[2] = ypr_avg[2] * 180/(AVERAGE_COUNT*M_PI);
-              
-              angle_values[0] = ypr_avg[0];
-              angle_values[1] = ypr_avg[1];
-              angle_values[2] = ypr_avg[2];
-              ypr_count = 0;
-              ypr_avg[0] = 0;
-              ypr_avg[1] = 0;
-              ypr_avg[2] = 0;
-              mpu.resetFIFO();       
-              return true;          
-            }
-        }
-        else{
-          //Serial.println("bad int status");
-        }
-      } 
-    }   
+      //    if (!dmpReady) return false;
+      //    
+      //    while(ypr_count < (AVERAGE_COUNT + 1)){
+      //      if (mpuInterrupt || fifoCount >= packetSize){  
+      //        // reset interrupt flag and get INT_STATUS byte
+      //        mpuInterrupt = false;
+      //        mpuIntStatus = mpu.getIntStatus();
+      //    
+      //        // get current FIFO count
+      //        fifoCount = mpu.getFIFOCount();
+      //        // check for overflow (this should never happen unless our code is too inefficient)
+      //        if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+      //            // reset so we can continue cleanly
+      //            mpu.resetFIFO();
+      //            
+      //            #ifdef DEBUG
+      //              Serial.println(F("FIFO overflow!"));
+      //            #endif
+      //            
+      //            return false;    
+      //        // otherwise, check for DMP data ready interrupt (this should happen frequently)
+      //        } else if (mpuIntStatus & 0x02) {
+      //            // wait for correct available data length, should be a VERY short wait
+      //            while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+      //    
+      //            // read a packet from FIFO
+      //            mpu.getFIFOBytes(fifoBuffer, packetSize);
+      //            
+      //            // track FIFO count here in case there is > 1 packet available
+      //            // (this lets us immediately read more without waiting for an interrupt)
+      //            fifoCount -= packetSize;
+      //            
+      //            // display Euler angles in degrees
+      //            mpu.dmpGetQuaternion(&q, fifoBuffer);
+      //            mpu.dmpGetGravity(&gravity, &q);
+      //            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      //            if (ypr_count < AVERAGE_COUNT) {
+      //              ypr_avg[0] += ypr[0];
+      //              ypr_avg[1] += ypr[1];
+      //              ypr_avg[2] += ypr[2];
+      //              ypr_count ++;
+      //            }else{
+      //              #ifdef DEBUG
+      //                Serial.print(interruptNum);
+      //                Serial.print(F("\t ypr\t"));
+      //                Serial.print(ypr_avg[0] * 180/(AVERAGE_COUNT*M_PI));
+      //                Serial.print(F("\t"));
+      //                Serial.print(ypr_avg[1] * 180/(AVERAGE_COUNT*M_PI));
+      //                Serial.print(F("\t"));
+      //                Serial.println(ypr_avg[2] * 180/(AVERAGE_COUNT*M_PI));
+      //              #endif
+      //  
+      //              ypr_avg[0] = ypr_avg[0] * 180/(AVERAGE_COUNT*M_PI);
+      //              ypr_avg[1] = ypr_avg[1] * 180/(AVERAGE_COUNT*M_PI);
+      //              ypr_avg[2] = ypr_avg[2] * 180/(AVERAGE_COUNT*M_PI);
+      //              
+      //              angle_values[0] = ypr_avg[0];
+      //              angle_values[1] = ypr_avg[1];
+      //              angle_values[2] = ypr_avg[2];
+      //              ypr_count = 0;
+      //              ypr_avg[0] = 0;
+      //              ypr_avg[1] = 0;
+      //              ypr_avg[2] = 0;
+      //              mpu.resetFIFO();       
+      //              return true;          
+      //            }
+      //        }
+      //        else{
+      //          //Serial.println("bad int status");
+      //        }
+      //      } 
+      //    }   
 }
 
 MPU6050* IMUController::getIMU(){
